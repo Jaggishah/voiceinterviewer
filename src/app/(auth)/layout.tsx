37831b2661d544d4c6cff84c817d10a585be57c3
Jaggishah <jugdeepshah@gmail.com>
2025-04-08 22:09:57 -0700

    Added setup for project

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+  children: React.ReactNode
+}
+const layout = ({children}:Props) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default layout
